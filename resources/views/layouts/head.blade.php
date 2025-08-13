bfa623d17635ad708b97309ad53a00e8e07938fb
santani423 <santani423@gmail.com>
2025-08-14 06:18:46 +0700

    revisi 14 ags 25

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,39 +1,37 @@
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <meta charset="UTF-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>@yield('title')</title>
+ <title>@yield('title')</title>
 
-<!-- Favicon -->
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/logo/logo.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/logo/logo.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/logo/logo.png') }}">
-<link rel="shortcut icon" href="{{ asset('assets/images/logo/logo.png') }}" type="image/x-icon">
-<link rel="icon" href="{{ asset('assets/images/logo/logo.png') }}" type="image/x-icon">
+ <!-- Favicon -->
+ <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/logo/logo.png') }}">
+ <!-- Jika punya favicon.ico -->
+ <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/logo/logo.png') }}">
 
-<!-- Bootstrap min css -->
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+ <!-- Bootstrap min css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
 
-<!-- Mean menu css -->
-<link rel="stylesheet" href="{{ asset('assets/css/meanmenu.css') }}" />
+ <!-- Mean menu css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/meanmenu.css') }}" />
 
-<!-- All min css (fontawesome, dll) -->
-<link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}" />
+ <!-- All min css (fontawesome, dll) -->
+ <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}" />
 
-<!-- Swiper bundle min css -->
-<link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}" />
+ <!-- Swiper bundle min css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}" />
 
-<!-- Magnific popup css -->
-<link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}" />
+ <!-- Magnific popup css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}" />
 
-<!-- Animate css -->
-<link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" />
+ <!-- Animate css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" />
 
-<!-- Nice select css -->
-<link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}" />
+ <!-- Nice select css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}" />
 
-<!-- Main style css -->
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+ <!-- Main style css -->
+ <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
 
-<!-- Tambahan CSS dari halaman lain -->
-@yield('css')
+ <!-- Tambahan CSS dari halaman lain -->
+ @yield('css')
